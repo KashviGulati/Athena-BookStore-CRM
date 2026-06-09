203b3675001bbb6a3e748d6a510d1f57f49101b2
Kashvi <kashvi.gulati23@lpu.in>
2026-06-09 23:32:03 +0530

    integrated order upload csv and customer summary endpoint

--- backend/crm/urls.py
+++ backend/crm/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import customer_list, customer_detail, upload_customers
+from .views import customer_list, customer_detail, upload_customers, upload_orders, customer_summary
 
 urlpatterns = [
     path("customers/", customer_list),
     path("customers/<int:customer_id>/",customer_detail),
     path("customers/upload/",upload_customers),
+    path("orders/upload/",upload_orders),
+    path("customers/<int:customer_id>/summary/",customer_summary),
 ]
--- backend/crm/views.py
+++ backend/crm/views.py
@@ -1,9 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .models import Customer
+from .models import Customer, Order
 from .serializers import CustomerSerializer
 from django.shortcuts import get_object_or_404
+from django.db.models import Sum
 
 import csv
 from io import TextIOWrapper
@@ -62,4 +63,77 @@ def upload_customers(request):
 
     return Response({
         "message": f"{count} customers uploaded"
+    })
+
+@api_view(["POST"])
+def upload_orders(request):
+    file = request.FILES.get("file")
+
+    if not file:
+        return Response(
+            {"error": "No file uploaded"},
+            status=400
+        )
+
+    csv_file = TextIOWrapper(
+        file.file,
+        encoding="utf-8"
+    )
+
+    reader = csv.DictReader(csv_file)
+
+    count = 0
+
+    for row in reader:
+
+        customer = Customer.objects.get(
+            id=row["customer_id"]
+        )
+
+        Order.objects.create(
+            customer=customer,
+            book_title=row["book_title"],
+            genre=row["genre"],
+            price=row["price"],
+            purchase_date=row["purchase_date"]
+        )
+
+        count += 1
+
+    return Response({
+        "message": f"{count} orders uploaded"
+    })
+
+
+@api_view(["GET"])
+def customer_summary(request, customer_id):
+    customer = get_object_or_404(
+        Customer,
+        id=customer_id
+    )
+
+    total_orders = customer.orders.count()
+
+    total_spend = (
+        customer.orders.aggregate(
+            total=Sum("price")
+        )["total"]
+        or 0
+    )
+
+    genres = list(
+        customer.orders.values_list(
+            "genre",
+            flat=True
+        ).distinct()
+    )
+
+    return Response({
+        "id": customer.id,
+        "name": customer.name,
+        "email": customer.email,
+        "phone": customer.phone,
+        "total_orders": total_orders,
+        "total_spend": total_spend,
+        "genres": genres
     })
